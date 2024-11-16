59e369331b9572b8868e8acc375390a7f715cf3f
Kevin Seldomridge <kevin.seldomridge@gmail.com>
2024-11-16 12:29:10 -0500

    Added comments so users understand script

--- execute.sh
+++ execute.sh
@@ -1,7 +1,17 @@
+# Script to launch Cyber Lab environment
+
+# Run compose.yaml file to build and launch containers user, reverse proxy, and attacker
+# Each container uses a dockerfile and installs dependencies/copys appropriate files
 docker-compose up -d
+
+# Copy public and private keys from reverse proxy to simulate stolen certs
 docker cp reverse_proxy:/etc/ssl/private/nginx-selfsigned.key .
 docker cp reverse_proxy:/etc/ssl/certs/nginx-selfsigned.crt .
+
+# Copy stolen certs to attackers machine so they can serve malicious webpage
 docker exec -i attacker sh -c 'cat > /simple-https-python-server/nginx-selfsigned.crt' < ./nginx-selfsigned.crt
 docker exec -i attacker sh -c 'cat > /simple-https-python-server/nginx-selfsigned.key' < ./nginx-selfsigned.key
+
+# Remove intermediate cert files from machine running this script
 rm -f nginx-selfsigned.crt
 rm -f nginx-selfsigned.key
