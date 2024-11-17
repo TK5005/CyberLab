7f899fa7ce88c1500bff8d53e3e93a49744d797a
Kevin Seldomridge <kevin.seldomridge@gmail.com>
2024-11-17 15:07:38 -0500

    Removed rebuild command when using docker-compose

--- execute.sh
+++ execute.sh
@@ -1,7 +1,6 @@
 # Script to launch Cyber Lab environment
 # Run compose.yaml file to build and launch containers user, reverse proxy, and attacker
 # Each container uses a dockerfile and installs dependencies/copys appropriate files
-docker-compose build --no-cache
 docker-compose up -d
 
 # Copy public and private keys from reverse proxy to simulate stolen certs
